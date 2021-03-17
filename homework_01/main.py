efa3d5e625296dd5cd788b535f79aa3a0fc11884
regmest <meschkova.regina@yandex.ru>
2021-03-18 02:24:36 +0300

    homework01 is done

--- homework_01/main.py
+++ homework_01/main.py
@@ -3,23 +3,36 @@
 Функции и структуры данных
 """
 
+"""
+функция, которая принимает N целых чисел,
+и возвращает список квадратов этих чисел
+"""
+def power_numbers(*number):
+    return [i ** 2 for i in number]
 
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    """
-
-
+"""
+функция, которая на вход принимает список из целых чисел,
+и возвращает только чётные/нечётные/простые числа
+(выбор производится передачей дополнительного аргумента)
+"""
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(num):
+    division_cnt = 0
+    for i in range(2, num):
+        if num % i == 0:
+            division_cnt += 1
+    if division_cnt == 0 and num > 1:
+        return True
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
-    """
+def filter_numbers(number_list, filter_type=PRIME):
+    even_numbers = [i for i in number_list if i % 2 == 0]
+    if filter_type == PRIME:
+        return list(filter(is_prime, number_list))
+    elif filter_type == EVEN:
+        return even_numbers
+    else:
+        return [i for i in number_list if i not in even_numbers]
