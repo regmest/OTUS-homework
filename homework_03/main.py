fcbd0c1779d0e4a45103242d9499d68c25d2afb9
regmest <meschkova.regina@yandex.ru>
2021-06-06 19:04:43 +0300

    homework_03 upload

--- homework_03/main.py
+++ homework_03/main.py
@@ -12,8 +12,11 @@
   (используйте полученные из запроса данные, передайте их в функцию для добавления в БД)
 - закрытие соединения с БД
 """
+
+# -*- coding: utf8 -*-
+
 import asyncio
-from models import Base, engine, User, Post, async_session
+from models import Base, engine, User, Post, async_session, conn
 from sqlalchemy.ext.asyncio import AsyncSession
 from loguru import logger
 from jsonplaceholder_requests import get_all_data
@@ -34,7 +37,17 @@ async def add_user_item(data):
         session: AsyncSession
 
         async with session.begin():
-            session.add(User(**user_data) for user_data in data)
+            for user_data in data:
+                # logger.info("Starting to add this user data: {}", user_data)
+                user = User(
+                    id=user_data["id"],
+                    name=user_data["name"],
+                    username=user_data["username"],
+                    email=user_data["email"],
+                    city=user_data["city"],
+                    phone=user_data["phone"],
+                    )
+                session.add(user)
 
     logger.info("Finishing to add user data into user table")
 
@@ -46,24 +59,39 @@ async def add_post_item(data):
         session: AsyncSession
 
         async with session.begin():
-            session.add(Post(**post_data) for post_data in data)
+            for post_data in data:
+                # logger.info("Starting to add this post data: {}", post_data)
+                post = Post(
+                    userId=post_data["userId"],
+                    id=post_data["id"],
+                    title=post_data["title"],
+                    body=post_data["body"]
+                    )
+                session.add(post)
 
     logger.info("Finishing to add post data into post table")
 
 
 async def async_main():
     logger.info("Starting async_main")
-
     await create_tables()
     user_data, post_data = await get_all_data()
-    await add_user_item(user_data)
-    await add_post_item(post_data)
-    # coros = [
-    #     add_user_item(user_data),
-    #     add_post_item(post_data)
-    # ]
-    # coro = asyncio.wait({asyncio.create_task(coro) for coro in coros})
-    # await coro
+    # await add_user_item(user_data)
+    # await add_post_item(post_data)
+    coros = [
+        add_user_item(user_data),
+        add_post_item(post_data)
+    ]
+    coro = asyncio.wait({asyncio.create_task(coro) for coro in coros})
+    await coro
+
+    # logger.info("Closing PG connection")
+    # conn.close()
+    # logger.info("CONNECTION INFO: {}", conn)
+
+    # await asyncio.sleep(0.1)  # чтобы обойти баг на windows https://github.com/encode/httpx/issues/914
+    # # Была ошибка: RuntimeError: Event loop is closed
+
     logger.info("Finishing async_main")
 
 
--- homework_03/models.py
+++ homework_03/models.py
@@ -8,7 +8,7 @@
 создайте связи relationship между моделями: User.posts и Post.user
 """
 
-import os
+# import os
 from datetime import datetime
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy import (
@@ -25,23 +25,35 @@ from sqlalchemy.orm import (
     sessionmaker,
 )
 
-PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") # or "postgresql+asyncpg://postgres:password@localhost/postgres"
+
+# PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI")
+
+PG_CONN_URI = "postgresql+asyncpg://postgres:password@localhost/postgres"  #  "postgresql+asyncpg://postgres:secretpassword@localhost:5432/postgres"
+
 engine = create_async_engine(PG_CONN_URI, echo=True)
 Base = declarative_base()
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False )
 
 
+# ---- Для теста на локальном PG ----------
+# conn = psycopg2.connect("dbname = db user=postgres password=16c808 port=5432 host=localhost")
+# PG_CONN_URI = "postgresql+asyncpg://postgres:16c808@localhost/db"
+# engine = create_async_engine(PG_CONN_URI, echo=True)
+# Base = declarative_base()
+# async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+# ------------------------------------------
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    name = Column(String(32))
-    username = Column(String(32))#, unique=True, nullable=False)
-    email = Column(String(32))#, unique=True)
-    city = Column(String(32))
-    phone = Column(String(32))#, unique=True)
-    # created_at = Column(DateTime, nullable=False, default=datetime.utcnow, server_default=func.now())
+    name = Column(String(100))
+    username = Column(String(100), unique=True, nullable=False)
+    email = Column(String(100), unique=True)
+    city = Column(String(100))
+    phone = Column(String(100), unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow, server_default=func.now())
 
-    # __mapper_args__ = {"eager_defaults": True}
+    __mapper_args__ = {"eager_defaults": True}
 
     post = relationship("Post", back_populates="user")
 
@@ -57,11 +69,11 @@ class Post(Base):
     __tablename__ = "posts"
     userId = Column(Integer, ForeignKey(User.id), nullable=False)
     id = Column(Integer, primary_key=True)
-    title = Column(String(32), nullable=False, default="", server_default="")
-    body = Column(String(1000), nullable=False, default="", server_default="")
-    # created_at = Column(DateTime, nullable=False, default=datetime.utcnow, server_default=func.now())
+    title = Column(String(1000), nullable=False, default="", server_default="")
+    body = Column(String(5000), nullable=False, default="", server_default="")
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow, server_default=func.now())
 
-    # __mapper_args__ = {"eager_defaults": True}
+    __mapper_args__ = {"eager_defaults": True}
 
     user = relationship(User, back_populates="post")
 
